11d080b8e15f5fc303ef608dafbade127f56950c
kelvinakpobome <kelvinakpobome@gmail.com>
2021-07-14 08:05:01 +0100

    removed the version constraints and quotes around vars types

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,7 +1,6 @@
 ## Ensures 2.0+ of the azurevm provider to get the azurerm_windows_virtual_machine resource and
 ## the other resources and capabilities
 provider "azurerm" {
-  version = "2.0.0"
   subscription_id = "${ var.subscription_id}"
   client_id = "${var.client_id}"
   client_secret = "${var.client_secret}"
@@ -9,41 +8,41 @@ provider "azurerm" {
 }
 
 variable "subscription_id" {
-    type = "string"
+    type = string
     #description = "Subscription ID of Azure user" 
     default = 
 }
 
 variable "client_id" {
-    type = "string"
+    type = string
      #description = "App ID of Azure user"
 }
 variable "client_secret" {
-    type = "string"
+    type = string
     #description = "App password of Azure user"
 }
 variable "tenant_id" {
-    type = "string"
+    type = string
     #description = "Tenant ID of Azure user"
 }
 variable "location" {
-    type = "string"
+    type = string
     #description = "location of instance"
 }
 
 variable "network_security_group" {
-    type = "string"
+    type = string
 }
 
 variable "resource_group" {
-    type = "string"
+    type = string
 }
 variable "virtual_network" {
-    type = "string"
+    type = string
 }
 
 variable "VM_number" {
-    type = "string"
+    type = number
 }
 
 
