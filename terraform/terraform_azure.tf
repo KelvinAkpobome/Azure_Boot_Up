7a740bcb4d59733fb4068c642717466889743d44
kelvinakpobome <kelvinakpobome@gmail.com>
2021-07-14 17:07:37 +0100

    fixes: removed the wait from playbook, added depends on in .tf

--- terraform/terraform_azure.tf
+++ terraform/terraform_azure.tf
@@ -175,7 +175,7 @@ resource "azurerm_linux_virtual_machine" "linuxVMs" {
 
 
 data "azurerm_public_ip" "vmIps"{
-  name = azurerm_public_ip.vmIps[count.index]
+  name = azurerm_public_ip.vmIps[terraform-${count.index}]
   resource_group_name = azurerm_resource_group.terraform-RG.name
   depends_on = [
   azurerm_public_ip.vmIps,
